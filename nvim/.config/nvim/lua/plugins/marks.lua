29eac9f337ebfd1ed9042ff8c92cebe461c76020
Cameron Malone <cameronmkm2@gmail.com>
2023-04-04 23:02:28 +1000

    added marks.nvim plugin with default bindings no opts

---
+++ nvim/.config/nvim/lua/plugins/marks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "chentoast/marks.nvim",
+    -- keymaps
+    -- mx: set mark x
+    -- m,: set next available mark
+    -- m;: toggle next available mark
+    -- dmx: delete mark x
+    -- dm-: delete marks on curr line
+    -- dm<space>: delete marks in buffer
+    -- m]: next mark
+    -- m[: prev mark
+    -- m:: preview mark
+  },
+}
