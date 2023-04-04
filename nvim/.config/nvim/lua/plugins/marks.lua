521bc892a91467d1f2ecea308a6d88068675f3f3
Cameron Malone <cameronmkm2@gmail.com>
2023-04-04 23:23:21 +1000

    added bookmark functionality to marks.nvim in opts

--- nvim/.config/nvim/lua/plugins/marks.lua
+++ nvim/.config/nvim/lua/plugins/marks.lua
@@ -1,6 +1,16 @@
 return {
   {
     "chentoast/marks.nvim",
+    opts = {
+      default_mappings = true,
+      bookmark_0 = {
+        sign = "⚑",
+        virt_text = "hello world",
+        -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
+        -- defaults to false.
+        annotate = false,
+      },
+    },
     -- keymaps
     -- mx: set mark x
     -- m,: set next available mark
