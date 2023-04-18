c436860573cb432a0cf48ccbeaf3d565e99045da
Cameron Malone <cameronmkm2@gmail.com>
2023-04-18 12:43:41 +1000

    added nvim-tmux-navigation plugin

---
+++ nvim/.config/nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,17 @@
+-- see https://github.com/alexghergh/nvim-tmux-navigation
+return {
+  {
+    "alexghergh/nvim-tmux-navigation",
+    opts = {
+      disable_when_zoomed = true,
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      },
+    },
+  },
+}
