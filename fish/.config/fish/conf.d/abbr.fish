42119a0fe640bc3871d5a79a8c9062aacfd765dc
Cameron Malone <cameronmkm2@gmail.com>
2023-04-24 01:50:06 +1000

    added some abbrs

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,9 +1,9 @@
 # program abbrs
 abbr vim nvim
-abbr mam minimamba
+abbr lg lazygit
 
 # place abbrs
-abbr dot "nvim ~/.dotfiles/"
+abbr dot "z dotfiles && nvim"
 
 # command abbrs
 abbr c clear
@@ -11,3 +11,19 @@ abbr cl clear
 abbr claer clear
 abbr clera clear
 abbr , z
+
+# tmux
+abbr tk "tmux kill-server"
+abbr ta "tmux a"
+abbr tat "tmux attach -t"
+abbr td "t dotfiles"
+abbr tn "tmux new -s (basename (pwd))"
+
+# micromamba
+abbr mam micromamba
+abbr mc "micromamba create"
+abbr ma "micromamba activate"
+abbr md "micromamba deactivate"
+abbr mae 'micromamba activate | fzf --query="$1" -m --preview "micromamba env list"'
+# man-style pages with tldr
+abbr toolong "tldr --list | fzf --header 'I ain readin allat' --reverse --preview 'tldr {1} --color=always' --preview-window=right,80% | xargs tldr"
