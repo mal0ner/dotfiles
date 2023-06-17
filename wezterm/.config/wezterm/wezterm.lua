0b0dfba0e5ae03260f3b62cfb29383445ce86b13
Cameron Malone <cameronmkm2@gmail.com>
2023-06-17 14:35:45 +1000

    added wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+config.font_size = 15
+config.color_scheme = "Everforest Dark (Gogh)"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+
+return config
