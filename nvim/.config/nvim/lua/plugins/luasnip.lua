32944d3cc48d3a4cb249ba6a05a2e4bd1289b90a
Cameron Malone <cameronmkm2@gmail.com>
2023-08-06 16:21:04 +1000

    luasnip keybinds

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,10 +1,33 @@
+-- return {}
 -- Use <tab> for completion and snippets (supertab)
 -- first: disable default <tab> and <s-tab> behavior in LuaSnip
 return {
   {
     "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
+    keys = {
+      {
+        "<C-j>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      {
+        "<C-j>",
+        function()
+          require("luasnip").jump(1)
+        end,
+        mode = "s",
+      },
+      {
+        "<C-k>",
+        function()
+          require("luasnip").jump(-1)
+        end,
+        mode = { "i", "s" },
+      },
+    },
   },
 }
