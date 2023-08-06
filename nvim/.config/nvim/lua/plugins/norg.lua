786979d87e7a572dd240672c3fa085a3a5b52fd0
Cameron Malone <cameronmkm2@gmail.com>
2023-08-06 16:18:45 +1000

    added some plugins

---
+++ nvim/.config/nvim/lua/plugins/norg.lua
@@ -0,0 +1,23 @@
+return {}
+-- return {
+--   {
+--     "nvim-neorg/neorg",
+--     build = ":Neorg sync-parsers",
+--     dependencies = { "nvim-lua/plenary.nvim" },
+--     config = function()
+--       require("neorg").setup({
+--         load = {
+--           ["core.defaults"] = {}, -- Loads default behaviour
+--           ["core.concealer"] = {}, -- Adds pretty icons to your documents
+--           ["core.dirman"] = { -- Manages Neorg workspaces
+--             config = {
+--               workspaces = {
+--                 school = "~/notes/school",
+--               },
+--             },
+--           },
+--         },
+--       })
+--     end,
+--   },
+-- }
