97a3613e4762c7a6d7f2564739aab10e02ca9583
Cameron Malone <cameronmkm2@gmail.com>
2023-08-06 16:19:22 +1000

    borders to LSP hover and CMP popup windows

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,7 +4,16 @@
 
 -- Set winbar
 vim.opt.winbar = "%=%m %f"
-vim.opt.guicursor = ""
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 100
+
+vim.opt.pumblend = 0
+-- Uncomment to change cursor to block in all modes
+-- vim.opt.guicursor = ""
 
 -- set line number highlighting off
-vim.cmd("hi! LineNr guibg=none")
+-- vim.cmd("hi! LineNr guibg=none")
+local float = { focusable = true, style = "minimal", border = "rounded" }
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, float)
--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -23,6 +23,10 @@ return {
             require("luasnip").lsp_expand(args.body)
           end,
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         mapping = cmp.mapping.preset.insert({
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -39,7 +43,7 @@ return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
-          { name = "buffer" },
+          { name = "buffer", keyword_length = 5 },
           { name = "path" },
         }),
         formatting = {
@@ -52,6 +56,7 @@ return {
           end,
         },
         experimental = {
+          native_menu = false,
           ghost_text = {
             hl_group = "LspCodeLens",
           },
