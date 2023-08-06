39c86b554d08f1194729a71067716e89abbd13a6
Cameron Malone <cameronmkm2@gmail.com>
2023-08-06 16:19:32 +1000

    ivy theme for telescope pickers

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,45 @@
 return {
   {
-    "telescope.nvim",
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
     opts = {
+      -- defaults = {},
       pickers = {
         find_files = {
-          theme = "dropdown",
+          theme = "ivy",
+          layout_config = {
+            height = 0.30,
+          },
+        },
+        oldfiles = {
+          theme = "ivy",
+          layout_config = {
+            height = 0.30,
+          },
+        },
+        live_grep = {
+          theme = "ivy",
+          layout_config = {
+            height = 0.30,
+          },
+        },
+        buffers = {
+          theme = "ivy",
+          layout_config = {
+            height = 0.30,
+          },
+        },
+        lsp_document_symbols = {
+          theme = "ivy",
+          layout_config = {
+            height = 0.30,
+          },
         },
       },
     },
