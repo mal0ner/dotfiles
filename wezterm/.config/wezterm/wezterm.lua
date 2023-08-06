e5bc2e5b4a95f5b28cdd014396045e5b37dc754d
Cameron Malone <cameronmkm2@gmail.com>
2023-08-06 16:18:21 +1000

    bunch of cool colorschemes and fonts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,11 +2,23 @@ local wezterm = require("wezterm")
 
 local config = {}
 
-config.font_size = 15
-config.color_scheme = "Everforest Dark (Gogh)"
-config.window_background_opacity = 0.8
+config.font_size = 18
+-- config.color_scheme = "Everforest Dark (Gogh)"
+-- config.color_scheme = "Nature Suede (terminal.sexy)"
+config.color_scheme = "Banana Blueberry"
+-- config.color_scheme = "Retro"
+-- config.color_scheme = "rose-pine"
+-- config.color_scheme = "Sequoia Monochrome"
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
+config.window_padding = {
+	left = 15,
+}
 
+-- config.font = wezterm.font("Iosevka Nerd Font")
+config.font = wezterm.font("CaskaydiaCove Nerd Font")
+-- config.font = wezterm.font("VictorMono Nerd Font", { weight = "Medium" })
+-- config.font = wezterm.font("GohuFont 11 Nerd Font")
 return config
