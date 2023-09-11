93a6350869861e2eb19fe38de8952490f16f36c8
Cameron Malone <cameronmkm2@gmail.com>
2023-09-12 01:04:01 +1000

    add some colorscheme stuff

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,14 +2,10 @@ local wezterm = require("wezterm")
 
 local config = {}
 
-config.font_size = 18
--- config.color_scheme = "Everforest Dark (Gogh)"
--- config.color_scheme = "Nature Suede (terminal.sexy)"
-config.color_scheme = "Banana Blueberry"
--- config.color_scheme = "Retro"
--- config.color_scheme = "rose-pine"
--- config.color_scheme = "Sequoia Monochrome"
-config.window_background_opacity = 0.95
+config.font_size = 20
+config.color_scheme = "Sequoia Monochrome"
+
+config.window_background_opacity = 1.0
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
@@ -17,8 +13,16 @@ config.window_padding = {
 	left = 15,
 }
 
--- config.font = wezterm.font("Iosevka Nerd Font")
-config.font = wezterm.font("CaskaydiaCove Nerd Font")
--- config.font = wezterm.font("VictorMono Nerd Font", { weight = "Medium" })
--- config.font = wezterm.font("GohuFont 11 Nerd Font")
+config.font = wezterm.font("PragmataPro Nerd Font")
+config.font_rules = {
+	{
+		-- use unpatched PragmataPro for italics
+		italic = true,
+		font = wezterm.font({
+			family = "PragmataPro",
+			style = "Italic",
+			italic = true,
+		}),
+	},
+}
 return config
